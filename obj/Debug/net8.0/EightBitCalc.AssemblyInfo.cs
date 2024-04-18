3cb717fe829f639f0b705ecbdcc6baaca294c040
Casper <casper.eide@gmail.com>
2024-04-18 14:42:06 +0200

    todo (in NOTES.txt) update

--- obj/Debug/net8.0/EightBitCalc.AssemblyInfo.cs
+++ obj/Debug/net8.0/EightBitCalc.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EightBitCalc")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b506a485216ff7d6dd348b253ea55d68c57522c8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cb124524c5cf8b21cce668026d3262fcacb3cf38")]
 [assembly: System.Reflection.AssemblyProductAttribute("EightBitCalc")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EightBitCalc")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
